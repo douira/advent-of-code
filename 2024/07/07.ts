c3296337d9c0337e22a525fcfc76d643815ab91e
douira <douira100@gmail.com>
2024-12-07 19:54:19 +0100

    day 7

---
+++ 2024/07/07.ts
@@ -0,0 +1,42 @@
+import fs from "fs"
+import path from "path"
+const input = fs
+	.readFileSync(
+		path.join(
+			__dirname,
+			`input_${path.basename(process.argv[1] || "").split(".")[0]}`
+		)
+	)
+	.toString()
+	.trim()
+	.split("\n")
+
+function* calc(
+	items: number[],
+	index: number = items.length - 1
+): Generator<number> {
+	const value = items[index] as number
+	if (index === 0) {
+		yield value
+	} else {
+		for (const next of calc(items, index - 1)) {
+			yield next + value
+			yield next * value
+		}
+	}
+}
+
+console.log(
+	input.reduce((sum, line) => {
+		const [x, ...items] = line.split(/\D+/).map(x => parseInt(x)) as [
+			number,
+			...number[]
+		]
+		for (const result of calc(items)) {
+			if (result === x) {
+				return sum + x
+			}
+		}
+		return sum
+	}, 0)
+)
